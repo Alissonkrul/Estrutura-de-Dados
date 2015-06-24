6f6442a4c7b26e98df6c827bc2fcdd8b38497ae5
alisson.krul@hotmail.com <08071996>
2015-06-24 16:17:33 -0300

    lol

--- Ordenação-ED/main.c
+++ Ordenação-ED/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct tabela{
+int tempotrocas;
+int tempoComparacoes;
+int tempoMedio;
+int tempoTotal;
+int comparacoesMelhorCaso;
+int trocasMelhorCaso;
+int comparacoesPiorCaso;
+int tempoMelhorCaso;
+int trocasPiorCaso;
+int tempoPiorCaso;
+}
+
 int main()
 {
     printf("Hello 2 the world!\n");
     return 0;
 }
+
