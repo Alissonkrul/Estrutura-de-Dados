8fb5c81e2a27b752e1d7e697999a81f0f5fb7b98
kindingerlek <kindingerlek@hotmail.com>
2015-06-30 20:59:01 -0300

    :D ordenados

--- Ordenação-ED/main.c
+++ Ordenação-ED/main.c
@@ -3,7 +3,7 @@
 #include <locale.h>
 #include <windows.h>
 #include <time.h>
-#define MAX 300
+#define MAX 30000
 
 struct Tabela{
 
@@ -29,20 +29,23 @@ float tempo;
 
 void imprimeResultado(struct Tabela tabela)
 {
-printf("\n______________________________________________________");
-printf("\nMetodo: %s",tabela.nome);
-printf("\nNumero de Trocas: %d",tabela.numeroTrocas);
-printf("\nNumero de Comparacoes: %d",tabela.numeroComparacoes);
-printf("\nTempo Total: %lf seg",tabela.tempoTotal);
+printf("\n______________________________________________________\n");
+printf("\n\tMetodo: %s",tabela.nome);
+printf("\n\n\tNumero de Comparacoes: %d",tabela.numeroComparacoes);
+printf("\n\tNumero de Trocas     : %d",tabela.numeroTrocas);
+printf("\n\tTempo: %1f seg",tabela.tempoTotal);
 
-printf("\nNumero de comparacoes melhor caso: %d",tabela.comparacoesMelhorCaso);
-printf("\nNumero de trocas Melhor Caso: %d",tabela.trocasMelhorCaso);
-printf("\nTempo melhor caso: %lf seg",tabela.tempoMelhorCaso);
+printf("\n\n\n                     MELHOR CASO                    \n");
 
-printf("\nNumero de comparacoes Pior caso: %d",tabela. comparacoesPiorCaso);
-printf("\nNumero de trocas Pior caso: %d",tabela.trocasPiorCaso);
-printf("\nTempo pior caso : %lf seg",tabela.tempoPiorCaso);
-printf("\n________________________________________________________\n");
+printf("\n\tNumero de comparacoes: %d",tabela.comparacoesMelhorCaso);
+printf("\n\tNumero de trocas     : %d",tabela.trocasMelhorCaso);
+printf("\n\tTempo: %1f seg",tabela.tempoMelhorCaso);
+
+printf("\n\n\n                       PIOR CASO                    \n");
+
+printf("\n\tNumero de comparacoes: %d",tabela. comparacoesPiorCaso);
+printf("\n\tNumero de trocas     : %d",tabela.trocasPiorCaso);
+printf("\n\tTempo: %1f seg",tabela.tempoPiorCaso );
 }
 int getArrayRand(int *vetor)
 {
@@ -65,7 +68,6 @@ void copyArray( int *from, int *to)
         to[i] = from[i];
 }
 
-
 void invertArray( int *vetor)
 {
     int aux, i=0, range = MAX -1;
@@ -87,32 +89,24 @@ int random(int min, int max)
 }
 
 
-struct Resultado bubbleSort(int *vetor)
+void bubbleSort(int *vetor, struct Resultado *resultado)
 {
-  int swap = 0;
+  int swap = 1;
   int end = MAX - 1;
   int i;
-  clock_t tInicial, tFinal;
-  double tempo;
   int aux;
 
-  struct Resultado resultado;
-
-  resultado.comparacoes = 0;
-  resultado.trocas = 0;
-  tInicial = clock();
-
-  while(end > 0 || swap == 1)
+  while(end > 0 && swap == 1)
   {
       swap = 0;
 
       for (i = 0; i < end; i++)
       {
-          resultado.comparacoes++;  // Numero de comparações ++
+          resultado->comparacoes++;  // Numero de comparações ++
 
           if(vetor[i] > vetor[i+1])
           {
-              resultado.trocas++;   // Numero de trocas ++
+              resultado->trocas++;   // Numero de trocas ++
 
               aux = vetor[i];
               vetor[i] = vetor[i+1];
@@ -123,52 +117,190 @@ struct Resultado bubbleSort(int *vetor)
       }
       end--;
   }
-  tFinal = clock(); // tempo fina
-  resultado.tempo = ( (double) (tFinal - tInicial) ) / CLOCKS_PER_SEC; ;
-  return resultado;
+
 }
 
+void shellSort(int *vetor, struct Resultado *resultado)
+{
+   int i = (MAX - 1) / 2;
+   int chave, k, aux;
+
+   while(i != 0)
+   {
+      do
+      {
+         chave = 1;
+         for(k = 0; k < MAX - i; ++k)
+         {
+            resultado->comparacoes++;  // Numero de comparações ++
+
+            if(vetor[k] > vetor[k + i])
+            {
+                resultado->trocas++;   // Numero de trocas ++
+
+                aux = vetor[k];
+                vetor[k] = vetor[k + i];
+                vetor[k + i] = aux;
+                chave = 0;
+            }
+
+         }
+      }while(chave == 0);
+      i = i / 2;
+   }
+}
+
+void quickSort(int *vetor, int left, int right, struct Resultado *resultado)
+{
+    int i, j, x, y;
+
+    i = left;
+    j = right;
+    x = vetor[(left + right) / 2];
+
+    while(i <= j) {
+        while(vetor[i] < x && i < right) {
+            i++;
+        }
+        while(vetor[j] > x && j > left) {
+            j--;
+        }
+
+        resultado->comparacoes++;
+
+        if(i <= j) {
+
+            resultado->trocas++;
+
+            y = vetor[i];
+            vetor[i] = vetor[j];
+            vetor[j] = y;
+            i++;
+            j--;
+        }
+    }
+
+    if(j > left) {
+        quickSort(vetor, left, j, resultado);
+    }
+    if(i < right) {
+        quickSort(vetor, i, right, resultado);
+    }
+}
+
+struct Resultado callOrderMethod(int *vetor, int op){
+
+  struct Resultado resultado;
+
+  resultado.comparacoes = 0;
+  resultado.trocas = 0;
+
+  clock_t tInicial, tFinal;
+
+  tInicial = clock();
+  switch(op)
+  {
+        case 0:
+            bubbleSort(vetor, &resultado);
+            break;
+
+        case 1:
+            shellSort(vetor, &resultado);
+            break;
+
+        case 2:
+            quickSort(vetor, 0 , MAX, &resultado);
+            break;
+  }
+
+  tFinal = clock(); // tempo fina
+  resultado.tempo = ( (double) (tFinal - tInicial) ) / CLOCKS_PER_SEC;
+  return resultado;
+};
+
+void preencheTabela(struct Tabela *tabela, char *nome, struct Resultado resultado, int caso)
+{
+    tabela->nome = nome;
+    switch(caso)
+    {
+        case 0:
+            tabela->numeroComparacoes = resultado.comparacoes ;
+            tabela->tempoTotal = resultado.tempo;
+            tabela->numeroTrocas = resultado.trocas;
+            break;
+        case 1:
+            tabela->comparacoesMelhorCaso = resultado.comparacoes ;
+            tabela->tempoMelhorCaso = resultado.tempo;
+            tabela->trocasMelhorCaso = resultado.trocas;
+            break;
+        case 2:
+            tabela->comparacoesPiorCaso = resultado.comparacoes ;
+            tabela->tempoPiorCaso = resultado.tempo;
+            tabela->trocasPiorCaso = resultado.trocas;
+            break;
+    }
+}
 
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
 
     struct Resultado resultado;
-    struct Tabela tabelaBubble;
+    struct Tabela tabela;
     //int vetor[] = {7,3,2,1,5,6,4,8,9,10};
     int vetor[MAX];
     int vetorShell[MAX];
     int vetorBubble[MAX];
-    int vetorLuke[MAX];
+    int vetorQuick[MAX];
     getArrayRand(&vetor);
     copyArray(&vetor, vetorBubble);
     copyArray(&vetor, vetorShell);
-    copyArray(&vetor, vetorLuke);
+    copyArray(&vetor, vetorQuick);
+
+    resultado = callOrderMethod(&vetorBubble, 0); //chama o método bubble;
+    preencheTabela(&tabela, "BubbleSort", resultado, 0); // preenche a tabela no caso normal;
+
+    resultado = callOrderMethod(&vetorBubble, 0); //chama o método bubble;
+    preencheTabela(&tabela, "BubbleSort", resultado, 1); // preenche a tabela no melhor caso;
+
+    invertArray(&vetorBubble);
+
+    resultado = callOrderMethod(&vetorBubble, 0); //chama o método bubble;
+    preencheTabela(&tabela, "BubbleSort", resultado, 2); // preenche a tabela no pior caso;
+
+    imprimeResultado(tabela);
 
 
-//-------------------------------------- Buble ---------------------------------------------//
-        tabelaBubble.nome = "Bubble";
-    // Normal Buble
-        resultado = bubbleSort(vetorBubble);
-        tabelaBubble.numeroComparacoes = resultado.comparacoes ;
-        tabelaBubble.tempoTotal = resultado.tempo;
-        tabelaBubble.numeroTrocas = resultado.trocas;
 
-    // Melhor Buble
-        resultado = bubbleSort(vetorBubble);
-        tabelaBubble.comparacoesMelhorCaso = resultado.comparacoes ;
-        tabelaBubble.tempoMelhorCaso = resultado.tempo;
-        tabelaBubble.trocasMelhorCaso = resultado.trocas;
+    //*
 
-    //Inverter array
-        invertArray(&vetorBubble);
-    // Pior Buble
-        resultado = bubbleSort(vetorBubble);
-        tabelaBubble.comparacoesPiorCaso = resultado.comparacoes ;
-        tabelaBubble.tempoPiorCaso = resultado.tempo;
-        tabelaBubble.trocasPiorCaso = resultado.trocas;
-        imprimeResultado(tabelaBubble);
-//-------------------------------------- Buble ---------------------------------------------//
+    resultado = callOrderMethod(&vetorShell, 1); //chama o método bubble;
+    preencheTabela(&tabela, "ShellSort", resultado, 0); // preenche a tabela no caso normal;
+
+    resultado = callOrderMethod(&vetorShell, 1); //chama o método bubble;
+    preencheTabela(&tabela, "ShellSort", resultado, 1); // preenche a tabela no melhor caso;
+
+    invertArray(&vetorShell);
+    resultado = callOrderMethod(&vetorShell, 1); //chama o método bubble;
+    preencheTabela(&tabela, "ShellSort", resultado, 2); // preenche a tabela no pior caso;
+
+    imprimeResultado(tabela);
+
+
+
+
+    resultado = callOrderMethod(&vetorQuick, 2); //chama o método bubble;
+    preencheTabela(&tabela, "QuickSort", resultado, 0); // preenche a tabela no caso normal;
+
+    resultado = callOrderMethod(&vetorQuick, 2); //chama o método bubble;
+    preencheTabela(&tabela, "QuickSort", resultado, 1); // preenche a tabela no melhor caso;
+
+    invertArray(&vetorQuick);
+    resultado = callOrderMethod(&vetorQuick, 2); //chama o método bubble;
+    preencheTabela(&tabela, "QuickSort", resultado, 2); // preenche a tabela no pior caso;
+
+    imprimeResultado(tabela);
+    //*/
 
 return 0;
 }
